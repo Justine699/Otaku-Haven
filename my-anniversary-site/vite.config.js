8b8cfad167fdd243f810f6d61115198488491e87
Justine699 <miraljustinemc@gmail.com>
2026-08-15 06:16:00 +0800

    Delete my-anniversary-site/vite.config.js

--- my-anniversary-site/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Otaku-Haven/', // <--- Ipalit ang eksaktong pangalan ng repository mo
-})
